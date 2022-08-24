be9db004d49e9479bbff844626b841686b211404
Trentan Jurkans <107039183+TRN10@users.noreply.github.com>
2022-08-24 21:12:15 +1000

    get question function added

--- assets/js/script.js
+++ assets/js/script.js
@@ -25,3 +25,33 @@ function startQuiz() {
 
     getQuestion();
 }
+
+function getQuestion() {
+
+    var currentQuestion = questions[currentQuestionsIndex];
+
+    //update title with current question
+    var titleEl = document.getElementById('quiz-question');
+    titleEl.textContent = currentQuestion.title;
+
+    //clear previous question choices
+    optionsEl.innerHTML = '';
+
+    //loop over choices
+    for (var i = 0; i < currentQuestion.options.length; i++) {
+        console.log(currentQuestion);
+        console.log(currentQuestion.options)
+
+        //create button for each choice
+        var choice = currentQuestion.options[i];
+        var choiceNode = document.createElement('button');
+        choiceNode.setAttribute('class', 'choice');
+        choiceNode.setAttribute('value', choice);
+
+        choiceNode.textContent = i + 1 + '. ' + choice;
+
+        //display on page
+        optionsEl.appendChild(choiceNode);
+
+    }
+}
